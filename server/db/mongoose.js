9c8ca6907031f81774bd7da39588fab113ad9d31
Paul <rep5989@gmail.com>
2018-06-12 16:15:08 +0300

    final fixing db connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,4 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoAppMongoose');
-
 module.exports = {mongoose};
